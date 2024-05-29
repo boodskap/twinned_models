8aaf679f7ee6fb2a5a6371d9fe3e7f65b8c1f43f
Keerthika <keerthikac25@gmail.com>
2024-05-29 12:55:07 +0530

    dial widget model

---
+++ lib/dial/dial.g.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dial.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$DeviceMultiFieldDialWidgetConfigImpl
+    _$$DeviceMultiFieldDialWidgetConfigImplFromJson(
+            Map<String, dynamic> json) =>
+        _$DeviceMultiFieldDialWidgetConfigImpl(
+          field: (json['field'] as List<dynamic>?)
+                  ?.map((e) => e as String)
+                  .toList() ??
+              const [],
+          ranges: (json['ranges'] as List<dynamic>?)
+                  ?.map((e) => Range.fromJson(e as Map<String, dynamic>))
+                  .toList() ??
+              const [],
+          bgColor: (json['bgColor'] as num?)?.toInt() ?? 0x1F000000,
+          deviceId: json['deviceId'] as String? ?? '',
+          title: json['title'] as String? ?? 'Radial Axes Widget',
+          titleFont: json['titleFont'] as Map<String, dynamic>? ??
+              const {
+                'fontFamily': 'Open Sans',
+                'fontSize': 30,
+                'fontColor': 0xFF000000,
+                'fontBold': true
+              },
+        );
+
+Map<String, dynamic> _$$DeviceMultiFieldDialWidgetConfigImplToJson(
+        _$DeviceMultiFieldDialWidgetConfigImpl instance) =>
+    <String, dynamic>{
+      'field': instance.field,
+      'ranges': instance.ranges,
+      'bgColor': instance.bgColor,
+      'deviceId': instance.deviceId,
+      'title': instance.title,
+      'titleFont': instance.titleFont,
+    };
