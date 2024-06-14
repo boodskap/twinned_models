c86d5644ab5f7a6d5bb90c8fd2bccb36341dfe87
Priyadharshini107 <priyadharshinit1998@gmail.com>
2024-06-14 14:25:44 +0530

    circle widget changes

--- lib/generic_air_quality/generic_air_quality_circle.freezed.dart
+++ lib/generic_air_quality/generic_air_quality_circle.freezed.dart
@@ -28,6 +28,11 @@ mixin _$GenericAirQualityCircleWidgetConfig {
       throw _privateConstructorUsedError;
   String get deviceId => throw _privateConstructorUsedError;
   set deviceId(String value) => throw _privateConstructorUsedError;
+  String get mainField => throw _privateConstructorUsedError;
+  set mainField(String value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get mainTextFont => throw _privateConstructorUsedError;
+  set mainTextFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
   Map<String, dynamic> get prefixFont => throw _privateConstructorUsedError;
   set prefixFont(Map<String, dynamic> value) =>
       throw _privateConstructorUsedError;
@@ -59,6 +64,12 @@ mixin _$GenericAirQualityCircleWidgetConfig {
   int get inactiveCircleBorderColor => throw _privateConstructorUsedError;
   set inactiveCircleBorderColor(int value) =>
       throw _privateConstructorUsedError;
+  double get horizontalSpacing => throw _privateConstructorUsedError;
+  set horizontalSpacing(double value) => throw _privateConstructorUsedError;
+  double get verticalSpacing => throw _privateConstructorUsedError;
+  set verticalSpacing(double value) => throw _privateConstructorUsedError;
+  double get imageSize => throw _privateConstructorUsedError;
+  set imageSize(double value) => throw _privateConstructorUsedError;
 
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
   @JsonKey(ignore: true)
@@ -79,6 +90,8 @@ abstract class $GenericAirQualityCircleWidgetConfigCopyWith<$Res> {
       {String title,
       Map<String, dynamic> titleFont,
       String deviceId,
+      String mainField,
+      Map<String, dynamic> mainTextFont,
       Map<String, dynamic> prefixFont,
       Map<String, dynamic> suffixFont,
       Map<String, dynamic> valueFont,
@@ -90,7 +103,10 @@ abstract class $GenericAirQualityCircleWidgetConfigCopyWith<$Res> {
       int activeCircleBorderColor,
       double inactiveCircleRadius,
       int inactiveCircleBGColor,
-      int inactiveCircleBorderColor});
+      int inactiveCircleBorderColor,
+      double horizontalSpacing,
+      double verticalSpacing,
+      double imageSize});
 }
 
 /// @nodoc
@@ -110,6 +126,8 @@ class _$GenericAirQualityCircleWidgetConfigCopyWithImpl<$Res,
     Object? title = null,
     Object? titleFont = null,
     Object? deviceId = null,
+    Object? mainField = null,
+    Object? mainTextFont = null,
     Object? prefixFont = null,
     Object? suffixFont = null,
     Object? valueFont = null,
@@ -122,6 +140,9 @@ class _$GenericAirQualityCircleWidgetConfigCopyWithImpl<$Res,
     Object? inactiveCircleRadius = null,
     Object? inactiveCircleBGColor = null,
     Object? inactiveCircleBorderColor = null,
+    Object? horizontalSpacing = null,
+    Object? verticalSpacing = null,
+    Object? imageSize = null,
   }) {
     return _then(_value.copyWith(
       title: null == title
@@ -136,6 +157,14 @@ class _$GenericAirQualityCircleWidgetConfigCopyWithImpl<$Res,
           ? _value.deviceId
           : deviceId // ignore: cast_nullable_to_non_nullable
               as String,
+      mainField: null == mainField
+          ? _value.mainField
+          : mainField // ignore: cast_nullable_to_non_nullable
+              as String,
+      mainTextFont: null == mainTextFont
+          ? _value.mainTextFont
+          : mainTextFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
       prefixFont: null == prefixFont
           ? _value.prefixFont
           : prefixFont // ignore: cast_nullable_to_non_nullable
@@ -184,6 +213,18 @@ class _$GenericAirQualityCircleWidgetConfigCopyWithImpl<$Res,
           ? _value.inactiveCircleBorderColor
           : inactiveCircleBorderColor // ignore: cast_nullable_to_non_nullable
               as int,
+      horizontalSpacing: null == horizontalSpacing
+          ? _value.horizontalSpacing
+          : horizontalSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      verticalSpacing: null == verticalSpacing
+          ? _value.verticalSpacing
+          : verticalSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
     ) as $Val);
   }
 }
@@ -201,6 +242,8 @@ abstract class _$$GenericAirQualityCircleWidgetConfigImplCopyWith<$Res>
       {String title,
       Map<String, dynamic> titleFont,
       String deviceId,
+      String mainField,
+      Map<String, dynamic> mainTextFont,
       Map<String, dynamic> prefixFont,
       Map<String, dynamic> suffixFont,
       Map<String, dynamic> valueFont,
@@ -212,7 +255,10 @@ abstract class _$$GenericAirQualityCircleWidgetConfigImplCopyWith<$Res>
       int activeCircleBorderColor,
       double inactiveCircleRadius,
       int inactiveCircleBGColor,
-      int inactiveCircleBorderColor});
+      int inactiveCircleBorderColor,
+      double horizontalSpacing,
+      double verticalSpacing,
+      double imageSize});
 }
 
 /// @nodoc
@@ -231,6 +277,8 @@ class __$$GenericAirQualityCircleWidgetConfigImplCopyWithImpl<$Res>
     Object? title = null,
     Object? titleFont = null,
     Object? deviceId = null,
+    Object? mainField = null,
+    Object? mainTextFont = null,
     Object? prefixFont = null,
     Object? suffixFont = null,
     Object? valueFont = null,
@@ -243,6 +291,9 @@ class __$$GenericAirQualityCircleWidgetConfigImplCopyWithImpl<$Res>
     Object? inactiveCircleRadius = null,
     Object? inactiveCircleBGColor = null,
     Object? inactiveCircleBorderColor = null,
+    Object? horizontalSpacing = null,
+    Object? verticalSpacing = null,
+    Object? imageSize = null,
   }) {
     return _then(_$GenericAirQualityCircleWidgetConfigImpl(
       title: null == title
@@ -257,6 +308,14 @@ class __$$GenericAirQualityCircleWidgetConfigImplCopyWithImpl<$Res>
           ? _value.deviceId
           : deviceId // ignore: cast_nullable_to_non_nullable
               as String,
+      mainField: null == mainField
+          ? _value.mainField
+          : mainField // ignore: cast_nullable_to_non_nullable
+              as String,
+      mainTextFont: null == mainTextFont
+          ? _value.mainTextFont
+          : mainTextFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
       prefixFont: null == prefixFont
           ? _value.prefixFont
           : prefixFont // ignore: cast_nullable_to_non_nullable
@@ -305,6 +364,18 @@ class __$$GenericAirQualityCircleWidgetConfigImplCopyWithImpl<$Res>
           ? _value.inactiveCircleBorderColor
           : inactiveCircleBorderColor // ignore: cast_nullable_to_non_nullable
               as int,
+      horizontalSpacing: null == horizontalSpacing
+          ? _value.horizontalSpacing
+          : horizontalSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      verticalSpacing: null == verticalSpacing
+          ? _value.verticalSpacing
+          : verticalSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
     ));
   }
 }
@@ -322,6 +393,13 @@ class _$GenericAirQualityCircleWidgetConfigImpl
         'fontBold': true
       },
       this.deviceId = '',
+      this.mainField = '',
+      this.mainTextFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 13,
+        'fontColor': 0xFFFFFFFF,
+        'fontBold': true
+      },
       this.prefixFont = const {
         'fontFamily': 'Open Sans',
         'fontSize': 12,
@@ -363,7 +441,10 @@ class _$GenericAirQualityCircleWidgetConfigImpl
       this.activeCircleBorderColor = 0xFF000000,
       this.inactiveCircleRadius = 40,
       this.inactiveCircleBGColor = 0xFF000000,
-      this.inactiveCircleBorderColor = 0xFF000000})
+      this.inactiveCircleBorderColor = 0xFF000000,
+      this.horizontalSpacing = 5,
+      this.verticalSpacing = 20,
+      this.imageSize = 25})
       : super._();
 
   factory _$GenericAirQualityCircleWidgetConfigImpl.fromJson(
@@ -379,6 +460,12 @@ class _$GenericAirQualityCircleWidgetConfigImpl
   @override
   @JsonKey()
   String deviceId;
+  @override
+  @JsonKey()
+  String mainField;
+  @override
+  @JsonKey()
+  Map<String, dynamic> mainTextFont;
   @override
   @JsonKey()
   Map<String, dynamic> prefixFont;
@@ -415,10 +502,19 @@ class _$GenericAirQualityCircleWidgetConfigImpl
   @override
   @JsonKey()
   int inactiveCircleBorderColor;
+  @override
+  @JsonKey()
+  double horizontalSpacing;
+  @override
+  @JsonKey()
+  double verticalSpacing;
+  @override
+  @JsonKey()
+  double imageSize;
 
   @override
   String toString() {
-    return 'GenericAirQualityCircleWidgetConfig(title: $title, titleFont: $titleFont, deviceId: $deviceId, prefixFont: $prefixFont, suffixFont: $suffixFont, valueFont: $valueFont, prefixMainFont: $prefixMainFont, suffixMainFont: $suffixMainFont, valueMainFont: $valueMainFont, activeCircleRadius: $activeCircleRadius, activeCircleBGColor: $activeCircleBGColor, activeCircleBorderColor: $activeCircleBorderColor, inactiveCircleRadius: $inactiveCircleRadius, inactiveCircleBGColor: $inactiveCircleBGColor, inactiveCircleBorderColor: $inactiveCircleBorderColor)';
+    return 'GenericAirQualityCircleWidgetConfig(title: $title, titleFont: $titleFont, deviceId: $deviceId, mainField: $mainField, mainTextFont: $mainTextFont, prefixFont: $prefixFont, suffixFont: $suffixFont, valueFont: $valueFont, prefixMainFont: $prefixMainFont, suffixMainFont: $suffixMainFont, valueMainFont: $valueMainFont, activeCircleRadius: $activeCircleRadius, activeCircleBGColor: $activeCircleBGColor, activeCircleBorderColor: $activeCircleBorderColor, inactiveCircleRadius: $inactiveCircleRadius, inactiveCircleBGColor: $inactiveCircleBGColor, inactiveCircleBorderColor: $inactiveCircleBorderColor, horizontalSpacing: $horizontalSpacing, verticalSpacing: $verticalSpacing, imageSize: $imageSize)';
   }
 
   @JsonKey(ignore: true)
@@ -440,22 +536,26 @@ class _$GenericAirQualityCircleWidgetConfigImpl
 abstract class _GenericAirQualityCircleWidgetConfig
     extends GenericAirQualityCircleWidgetConfig {
   factory _GenericAirQualityCircleWidgetConfig(
-          {String title,
-          Map<String, dynamic> titleFont,
-          String deviceId,
-          Map<String, dynamic> prefixFont,
-          Map<String, dynamic> suffixFont,
-          Map<String, dynamic> valueFont,
-          Map<String, dynamic> prefixMainFont,
-          Map<String, dynamic> suffixMainFont,
-          Map<String, dynamic> valueMainFont,
-          double activeCircleRadius,
-          int activeCircleBGColor,
-          int activeCircleBorderColor,
-          double inactiveCircleRadius,
-          int inactiveCircleBGColor,
-          int inactiveCircleBorderColor}) =
-      _$GenericAirQualityCircleWidgetConfigImpl;
+      {String title,
+      Map<String, dynamic> titleFont,
+      String deviceId,
+      String mainField,
+      Map<String, dynamic> mainTextFont,
+      Map<String, dynamic> prefixFont,
+      Map<String, dynamic> suffixFont,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> prefixMainFont,
+      Map<String, dynamic> suffixMainFont,
+      Map<String, dynamic> valueMainFont,
+      double activeCircleRadius,
+      int activeCircleBGColor,
+      int activeCircleBorderColor,
+      double inactiveCircleRadius,
+      int inactiveCircleBGColor,
+      int inactiveCircleBorderColor,
+      double horizontalSpacing,
+      double verticalSpacing,
+      double imageSize}) = _$GenericAirQualityCircleWidgetConfigImpl;
   _GenericAirQualityCircleWidgetConfig._() : super._();
 
   factory _GenericAirQualityCircleWidgetConfig.fromJson(
@@ -471,6 +571,12 @@ abstract class _GenericAirQualityCircleWidgetConfig
   @override
   String get deviceId;
   set deviceId(String value);
+  @override
+  String get mainField;
+  set mainField(String value);
+  @override
+  Map<String, dynamic> get mainTextFont;
+  set mainTextFont(Map<String, dynamic> value);
   @override
   Map<String, dynamic> get prefixFont;
   set prefixFont(Map<String, dynamic> value);
@@ -507,6 +613,15 @@ abstract class _GenericAirQualityCircleWidgetConfig
   @override
   int get inactiveCircleBorderColor;
   set inactiveCircleBorderColor(int value);
+  @override
+  double get horizontalSpacing;
+  set horizontalSpacing(double value);
+  @override
+  double get verticalSpacing;
+  set verticalSpacing(double value);
+  @override
+  double get imageSize;
+  set imageSize(double value);
   @override
   @JsonKey(ignore: true)
   _$$GenericAirQualityCircleWidgetConfigImplCopyWith<
