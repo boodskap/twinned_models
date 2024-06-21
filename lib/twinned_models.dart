61ec13008a453f714534f8d106d47db283833cd0
Keerthika <keerthikac25@gmail.com>
2024-06-21 18:42:33 +0530

    Code Changes

--- lib/twinned_models.dart
+++ lib/twinned_models.dart
@@ -8,4 +8,5 @@ export 'generic_value_card/generic_value_card.dart';
 export 'multi_device_field_page/multi_device_field_page.dart';
 export 'multi_device_field_card/multi_device_field_card.dart';
 export 'multi_device_single_field_pie_chart/multi_device_single_field_pie_chart.dart';
+export 'device_field_scatter_chart/device_field_scatter_chart.dart';
 export 'multi_device_multi_field_bar_chart/multi_device_multi_field_bar_chart.dart';
