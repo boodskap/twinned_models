7021ab5ab9567a854a71c5f50c2dfc696db23f7c
Thirumalvalavan <thirumalvalavan99@gmail.com>
2024-07-01 18:41:55 +0530

    Humidity progress bar models added

---
+++ lib/humidity_progress_bar/humidity_progress_bar.freezed.dart
@@ -0,0 +1,305 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'humidity_progress_bar.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+HumidityProgressBarWidgetConfig _$HumidityProgressBarWidgetConfigFromJson(
+    Map<String, dynamic> json) {
+  return _HumidityProgressBarWidgetConfig.fromJson(json);
+}
+
+/// @nodoc
+mixin _$HumidityProgressBarWidgetConfig {
+  String get deviceId => throw _privateConstructorUsedError;
+  set deviceId(String value) => throw _privateConstructorUsedError;
+  String get field => throw _privateConstructorUsedError;
+  set field(String value) => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  set title(String value) => throw _privateConstructorUsedError;
+  int get backgroundColor => throw _privateConstructorUsedError;
+  set backgroundColor(int value) => throw _privateConstructorUsedError;
+  int get valueColor => throw _privateConstructorUsedError;
+  set valueColor(int value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get valueFont => throw _privateConstructorUsedError;
+  set valueFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get titleFont => throw _privateConstructorUsedError;
+  set titleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $HumidityProgressBarWidgetConfigCopyWith<HumidityProgressBarWidgetConfig>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $HumidityProgressBarWidgetConfigCopyWith<$Res> {
+  factory $HumidityProgressBarWidgetConfigCopyWith(
+          HumidityProgressBarWidgetConfig value,
+          $Res Function(HumidityProgressBarWidgetConfig) then) =
+      _$HumidityProgressBarWidgetConfigCopyWithImpl<$Res,
+          HumidityProgressBarWidgetConfig>;
+  @useResult
+  $Res call(
+      {String deviceId,
+      String field,
+      String title,
+      int backgroundColor,
+      int valueColor,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> titleFont});
+}
+
+/// @nodoc
+class _$HumidityProgressBarWidgetConfigCopyWithImpl<$Res,
+        $Val extends HumidityProgressBarWidgetConfig>
+    implements $HumidityProgressBarWidgetConfigCopyWith<$Res> {
+  _$HumidityProgressBarWidgetConfigCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? deviceId = null,
+    Object? field = null,
+    Object? title = null,
+    Object? backgroundColor = null,
+    Object? valueColor = null,
+    Object? valueFont = null,
+    Object? titleFont = null,
+  }) {
+    return _then(_value.copyWith(
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      field: null == field
+          ? _value.field
+          : field // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      backgroundColor: null == backgroundColor
+          ? _value.backgroundColor
+          : backgroundColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      valueColor: null == valueColor
+          ? _value.valueColor
+          : valueColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$HumidityProgressBarWidgetConfigImplCopyWith<$Res>
+    implements $HumidityProgressBarWidgetConfigCopyWith<$Res> {
+  factory _$$HumidityProgressBarWidgetConfigImplCopyWith(
+          _$HumidityProgressBarWidgetConfigImpl value,
+          $Res Function(_$HumidityProgressBarWidgetConfigImpl) then) =
+      __$$HumidityProgressBarWidgetConfigImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String deviceId,
+      String field,
+      String title,
+      int backgroundColor,
+      int valueColor,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> titleFont});
+}
+
+/// @nodoc
+class __$$HumidityProgressBarWidgetConfigImplCopyWithImpl<$Res>
+    extends _$HumidityProgressBarWidgetConfigCopyWithImpl<$Res,
+        _$HumidityProgressBarWidgetConfigImpl>
+    implements _$$HumidityProgressBarWidgetConfigImplCopyWith<$Res> {
+  __$$HumidityProgressBarWidgetConfigImplCopyWithImpl(
+      _$HumidityProgressBarWidgetConfigImpl _value,
+      $Res Function(_$HumidityProgressBarWidgetConfigImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? deviceId = null,
+    Object? field = null,
+    Object? title = null,
+    Object? backgroundColor = null,
+    Object? valueColor = null,
+    Object? valueFont = null,
+    Object? titleFont = null,
+  }) {
+    return _then(_$HumidityProgressBarWidgetConfigImpl(
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      field: null == field
+          ? _value.field
+          : field // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      backgroundColor: null == backgroundColor
+          ? _value.backgroundColor
+          : backgroundColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      valueColor: null == valueColor
+          ? _value.valueColor
+          : valueColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$HumidityProgressBarWidgetConfigImpl
+    extends _HumidityProgressBarWidgetConfig {
+  _$HumidityProgressBarWidgetConfigImpl(
+      {this.deviceId = '',
+      this.field = '',
+      this.title = '',
+      this.backgroundColor = 0xFFB3E5FC,
+      this.valueColor = 0XFF7DA9E1,
+      this.valueFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 12,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.titleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 18,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      }})
+      : super._();
+
+  factory _$HumidityProgressBarWidgetConfigImpl.fromJson(
+          Map<String, dynamic> json) =>
+      _$$HumidityProgressBarWidgetConfigImplFromJson(json);
+
+  @override
+  @JsonKey()
+  String deviceId;
+  @override
+  @JsonKey()
+  String field;
+  @override
+  @JsonKey()
+  String title;
+  @override
+  @JsonKey()
+  int backgroundColor;
+  @override
+  @JsonKey()
+  int valueColor;
+  @override
+  @JsonKey()
+  Map<String, dynamic> valueFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> titleFont;
+
+  @override
+  String toString() {
+    return 'HumidityProgressBarWidgetConfig(deviceId: $deviceId, field: $field, title: $title, backgroundColor: $backgroundColor, valueColor: $valueColor, valueFont: $valueFont, titleFont: $titleFont)';
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$HumidityProgressBarWidgetConfigImplCopyWith<
+          _$HumidityProgressBarWidgetConfigImpl>
+      get copyWith => __$$HumidityProgressBarWidgetConfigImplCopyWithImpl<
+          _$HumidityProgressBarWidgetConfigImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$HumidityProgressBarWidgetConfigImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _HumidityProgressBarWidgetConfig
+    extends HumidityProgressBarWidgetConfig {
+  factory _HumidityProgressBarWidgetConfig(
+      {String deviceId,
+      String field,
+      String title,
+      int backgroundColor,
+      int valueColor,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> titleFont}) = _$HumidityProgressBarWidgetConfigImpl;
+  _HumidityProgressBarWidgetConfig._() : super._();
+
+  factory _HumidityProgressBarWidgetConfig.fromJson(Map<String, dynamic> json) =
+      _$HumidityProgressBarWidgetConfigImpl.fromJson;
+
+  @override
+  String get deviceId;
+  set deviceId(String value);
+  @override
+  String get field;
+  set field(String value);
+  @override
+  String get title;
+  set title(String value);
+  @override
+  int get backgroundColor;
+  set backgroundColor(int value);
+  @override
+  int get valueColor;
+  set valueColor(int value);
+  @override
+  Map<String, dynamic> get valueFont;
+  set valueFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get titleFont;
+  set titleFont(Map<String, dynamic> value);
+  @override
+  @JsonKey(ignore: true)
+  _$$HumidityProgressBarWidgetConfigImplCopyWith<
+          _$HumidityProgressBarWidgetConfigImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
