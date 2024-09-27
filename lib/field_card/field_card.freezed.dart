98f71267f2ed5fa402e78d4397fddd278a3910a2
Priyadharshini107 <priyadharshinit1998@gmail.com>
2024-09-27 10:58:39 +0530

    field card widget config

---
+++ lib/field_card/field_card.freezed.dart
@@ -0,0 +1,463 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'field_card.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+FieldCardWidgetConfig _$FieldCardWidgetConfigFromJson(
+    Map<String, dynamic> json) {
+  return _FieldCardWidgetConfig.fromJson(json);
+}
+
+/// @nodoc
+mixin _$FieldCardWidgetConfig {
+  String get deviceId => throw _privateConstructorUsedError;
+  set deviceId(String value) => throw _privateConstructorUsedError;
+  String get field => throw _privateConstructorUsedError;
+  set field(String value) => throw _privateConstructorUsedError;
+  String get image => throw _privateConstructorUsedError;
+  set image(String value) => throw _privateConstructorUsedError;
+  int get topSectionColor => throw _privateConstructorUsedError;
+  set topSectionColor(int value) => throw _privateConstructorUsedError;
+  int get bottomSectionColor => throw _privateConstructorUsedError;
+  set bottomSectionColor(int value) => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  set title(String value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get valueFont => throw _privateConstructorUsedError;
+  set valueFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get titleFont => throw _privateConstructorUsedError;
+  set titleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get headingFont => throw _privateConstructorUsedError;
+  set headingFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get contentFont => throw _privateConstructorUsedError;
+  set contentFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  double get width => throw _privateConstructorUsedError;
+  set width(double value) => throw _privateConstructorUsedError;
+  double get height => throw _privateConstructorUsedError;
+  set height(double value) => throw _privateConstructorUsedError;
+  double get topSectionHeight => throw _privateConstructorUsedError;
+  set topSectionHeight(double value) => throw _privateConstructorUsedError;
+  double get imageSize => throw _privateConstructorUsedError;
+  set imageSize(double value) => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $FieldCardWidgetConfigCopyWith<FieldCardWidgetConfig> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $FieldCardWidgetConfigCopyWith<$Res> {
+  factory $FieldCardWidgetConfigCopyWith(FieldCardWidgetConfig value,
+          $Res Function(FieldCardWidgetConfig) then) =
+      _$FieldCardWidgetConfigCopyWithImpl<$Res, FieldCardWidgetConfig>;
+  @useResult
+  $Res call(
+      {String deviceId,
+      String field,
+      String image,
+      int topSectionColor,
+      int bottomSectionColor,
+      String title,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> headingFont,
+      Map<String, dynamic> contentFont,
+      double width,
+      double height,
+      double topSectionHeight,
+      double imageSize});
+}
+
+/// @nodoc
+class _$FieldCardWidgetConfigCopyWithImpl<$Res,
+        $Val extends FieldCardWidgetConfig>
+    implements $FieldCardWidgetConfigCopyWith<$Res> {
+  _$FieldCardWidgetConfigCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? deviceId = null,
+    Object? field = null,
+    Object? image = null,
+    Object? topSectionColor = null,
+    Object? bottomSectionColor = null,
+    Object? title = null,
+    Object? valueFont = null,
+    Object? titleFont = null,
+    Object? headingFont = null,
+    Object? contentFont = null,
+    Object? width = null,
+    Object? height = null,
+    Object? topSectionHeight = null,
+    Object? imageSize = null,
+  }) {
+    return _then(_value.copyWith(
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      field: null == field
+          ? _value.field
+          : field // ignore: cast_nullable_to_non_nullable
+              as String,
+      image: null == image
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as String,
+      topSectionColor: null == topSectionColor
+          ? _value.topSectionColor
+          : topSectionColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      bottomSectionColor: null == bottomSectionColor
+          ? _value.bottomSectionColor
+          : bottomSectionColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      headingFont: null == headingFont
+          ? _value.headingFont
+          : headingFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      contentFont: null == contentFont
+          ? _value.contentFont
+          : contentFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      width: null == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as double,
+      height: null == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as double,
+      topSectionHeight: null == topSectionHeight
+          ? _value.topSectionHeight
+          : topSectionHeight // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$FieldCardWidgetConfigImplCopyWith<$Res>
+    implements $FieldCardWidgetConfigCopyWith<$Res> {
+  factory _$$FieldCardWidgetConfigImplCopyWith(
+          _$FieldCardWidgetConfigImpl value,
+          $Res Function(_$FieldCardWidgetConfigImpl) then) =
+      __$$FieldCardWidgetConfigImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String deviceId,
+      String field,
+      String image,
+      int topSectionColor,
+      int bottomSectionColor,
+      String title,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> headingFont,
+      Map<String, dynamic> contentFont,
+      double width,
+      double height,
+      double topSectionHeight,
+      double imageSize});
+}
+
+/// @nodoc
+class __$$FieldCardWidgetConfigImplCopyWithImpl<$Res>
+    extends _$FieldCardWidgetConfigCopyWithImpl<$Res,
+        _$FieldCardWidgetConfigImpl>
+    implements _$$FieldCardWidgetConfigImplCopyWith<$Res> {
+  __$$FieldCardWidgetConfigImplCopyWithImpl(_$FieldCardWidgetConfigImpl _value,
+      $Res Function(_$FieldCardWidgetConfigImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? deviceId = null,
+    Object? field = null,
+    Object? image = null,
+    Object? topSectionColor = null,
+    Object? bottomSectionColor = null,
+    Object? title = null,
+    Object? valueFont = null,
+    Object? titleFont = null,
+    Object? headingFont = null,
+    Object? contentFont = null,
+    Object? width = null,
+    Object? height = null,
+    Object? topSectionHeight = null,
+    Object? imageSize = null,
+  }) {
+    return _then(_$FieldCardWidgetConfigImpl(
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      field: null == field
+          ? _value.field
+          : field // ignore: cast_nullable_to_non_nullable
+              as String,
+      image: null == image
+          ? _value.image
+          : image // ignore: cast_nullable_to_non_nullable
+              as String,
+      topSectionColor: null == topSectionColor
+          ? _value.topSectionColor
+          : topSectionColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      bottomSectionColor: null == bottomSectionColor
+          ? _value.bottomSectionColor
+          : bottomSectionColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      headingFont: null == headingFont
+          ? _value.headingFont
+          : headingFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      contentFont: null == contentFont
+          ? _value.contentFont
+          : contentFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      width: null == width
+          ? _value.width
+          : width // ignore: cast_nullable_to_non_nullable
+              as double,
+      height: null == height
+          ? _value.height
+          : height // ignore: cast_nullable_to_non_nullable
+              as double,
+      topSectionHeight: null == topSectionHeight
+          ? _value.topSectionHeight
+          : topSectionHeight // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$FieldCardWidgetConfigImpl extends _FieldCardWidgetConfig {
+  _$FieldCardWidgetConfigImpl(
+      {this.deviceId = '',
+      this.field = '',
+      this.image = '',
+      this.topSectionColor = 0xFF000000,
+      this.bottomSectionColor = 0xFF000000,
+      this.title = '',
+      this.valueFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 30,
+        'fontColor': 0xFFFFFFFF,
+        'fontBold': true
+      },
+      this.titleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 16,
+        'fontColor': 0xFF000000,
+        'fontBold': false
+      },
+      this.headingFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 16,
+        'fontColor': 0xFFFFFFFF,
+        'fontBold': false
+      },
+      this.contentFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 20,
+        'fontColor': 0xFF000000,
+        'fontBold': false
+      },
+      this.width = 350,
+      this.height = 350,
+      this.topSectionHeight = 200,
+      this.imageSize = 50})
+      : super._();
+
+  factory _$FieldCardWidgetConfigImpl.fromJson(Map<String, dynamic> json) =>
+      _$$FieldCardWidgetConfigImplFromJson(json);
+
+  @override
+  @JsonKey()
+  String deviceId;
+  @override
+  @JsonKey()
+  String field;
+  @override
+  @JsonKey()
+  String image;
+  @override
+  @JsonKey()
+  int topSectionColor;
+  @override
+  @JsonKey()
+  int bottomSectionColor;
+  @override
+  @JsonKey()
+  String title;
+  @override
+  @JsonKey()
+  Map<String, dynamic> valueFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> titleFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> headingFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> contentFont;
+  @override
+  @JsonKey()
+  double width;
+  @override
+  @JsonKey()
+  double height;
+  @override
+  @JsonKey()
+  double topSectionHeight;
+  @override
+  @JsonKey()
+  double imageSize;
+
+  @override
+  String toString() {
+    return 'FieldCardWidgetConfig(deviceId: $deviceId, field: $field, image: $image, topSectionColor: $topSectionColor, bottomSectionColor: $bottomSectionColor, title: $title, valueFont: $valueFont, titleFont: $titleFont, headingFont: $headingFont, contentFont: $contentFont, width: $width, height: $height, topSectionHeight: $topSectionHeight, imageSize: $imageSize)';
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$FieldCardWidgetConfigImplCopyWith<_$FieldCardWidgetConfigImpl>
+      get copyWith => __$$FieldCardWidgetConfigImplCopyWithImpl<
+          _$FieldCardWidgetConfigImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$FieldCardWidgetConfigImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _FieldCardWidgetConfig extends FieldCardWidgetConfig {
+  factory _FieldCardWidgetConfig(
+      {String deviceId,
+      String field,
+      String image,
+      int topSectionColor,
+      int bottomSectionColor,
+      String title,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> headingFont,
+      Map<String, dynamic> contentFont,
+      double width,
+      double height,
+      double topSectionHeight,
+      double imageSize}) = _$FieldCardWidgetConfigImpl;
+  _FieldCardWidgetConfig._() : super._();
+
+  factory _FieldCardWidgetConfig.fromJson(Map<String, dynamic> json) =
+      _$FieldCardWidgetConfigImpl.fromJson;
+
+  @override
+  String get deviceId;
+  set deviceId(String value);
+  @override
+  String get field;
+  set field(String value);
+  @override
+  String get image;
+  set image(String value);
+  @override
+  int get topSectionColor;
+  set topSectionColor(int value);
+  @override
+  int get bottomSectionColor;
+  set bottomSectionColor(int value);
+  @override
+  String get title;
+  set title(String value);
+  @override
+  Map<String, dynamic> get valueFont;
+  set valueFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get titleFont;
+  set titleFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get headingFont;
+  set headingFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get contentFont;
+  set contentFont(Map<String, dynamic> value);
+  @override
+  double get width;
+  set width(double value);
+  @override
+  double get height;
+  set height(double value);
+  @override
+  double get topSectionHeight;
+  set topSectionHeight(double value);
+  @override
+  double get imageSize;
+  set imageSize(double value);
+  @override
+  @JsonKey(ignore: true)
+  _$$FieldCardWidgetConfigImplCopyWith<_$FieldCardWidgetConfigImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
