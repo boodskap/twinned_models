f1a28adef3b1c116fa4990eb0ad27075cc5212c1
Keerthika <keerthikac25@gmail.com>
2024-10-07 17:08:21 +0530

    range gauge value font changes

--- lib/generic_up_down_pentagon/generic_up_down_pentagon.freezed.dart
+++ lib/generic_up_down_pentagon/generic_up_down_pentagon.freezed.dart
@@ -60,12 +60,8 @@ mixin _$GenericUpDownPentagonWidgetConfig {
   double get imageSize => throw _privateConstructorUsedError;
   set imageSize(double value) => throw _privateConstructorUsedError;
 
-  /// Serializes this GenericUpDownPentagonWidgetConfig to a JSON map.
   Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
-
-  /// Create a copy of GenericUpDownPentagonWidgetConfig
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
+  @JsonKey(ignore: true)
   $GenericUpDownPentagonWidgetConfigCopyWith<GenericUpDownPentagonWidgetConfig>
       get copyWith => throw _privateConstructorUsedError;
 }
@@ -107,8 +103,6 @@ class _$GenericUpDownPentagonWidgetConfigCopyWithImpl<$Res,
   // ignore: unused_field
   final $Res Function($Val) _then;
 
-  /// Create a copy of GenericUpDownPentagonWidgetConfig
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
   $Res call({
@@ -230,8 +224,6 @@ class __$$GenericUpDownPentagonWidgetConfigImplCopyWithImpl<$Res>
       $Res Function(_$GenericUpDownPentagonWidgetConfigImpl) _then)
       : super(_value, _then);
 
-  /// Create a copy of GenericUpDownPentagonWidgetConfig
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
   $Res call({
@@ -433,9 +425,7 @@ class _$GenericUpDownPentagonWidgetConfigImpl
     return 'GenericUpDownPentagonWidgetConfig(title: $title, titleFont: $titleFont, subTitle: $subTitle, subTitleFont: $subTitleFont, deviceId: $deviceId, prefixFont: $prefixFont, suffixFont: $suffixFont, valueFont: $valueFont, prefixMainFont: $prefixMainFont, suffixMainFont: $suffixMainFont, valueMainFont: $valueMainFont, upperPentagonBGColor: $upperPentagonBGColor, downPentagonBGColor: $downPentagonBGColor, horizontalSpacing: $horizontalSpacing, imageSize: $imageSize)';
   }
 
-  /// Create a copy of GenericUpDownPentagonWidgetConfig
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
+  @JsonKey(ignore: true)
   @override
   @pragma('vm:prefer-inline')
   _$$GenericUpDownPentagonWidgetConfigImplCopyWith<
@@ -520,11 +510,8 @@ abstract class _GenericUpDownPentagonWidgetConfig
   @override
   double get imageSize;
   set imageSize(double value);
-
-  /// Create a copy of GenericUpDownPentagonWidgetConfig
-  /// with the given fields replaced by the non-null parameter values.
   @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
+  @JsonKey(ignore: true)
   _$$GenericUpDownPentagonWidgetConfigImplCopyWith<
           _$GenericUpDownPentagonWidgetConfigImpl>
       get copyWith => throw _privateConstructorUsedError;
