cd7d1ac54e329df7be1d07c561eed2ea838ad497
Priyadharshini107 <priyadharshinit1998@gmail.com>
2024-10-07 12:25:15 +0530

    generic device card

---
+++ lib/field_card/generic_device_card.freezed.dart
@@ -0,0 +1,378 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'generic_device_card.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+GenericDeviceCardWidgetConfig _$GenericDeviceCardWidgetConfigFromJson(
+    Map<String, dynamic> json) {
+  return _GenericDeviceCardWidgetConfig.fromJson(json);
+}
+
+/// @nodoc
+mixin _$GenericDeviceCardWidgetConfig {
+  Map<String, dynamic> get titleFont => throw _privateConstructorUsedError;
+  set titleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  String get modelId => throw _privateConstructorUsedError;
+  set modelId(String value) => throw _privateConstructorUsedError;
+  int get borderColor => throw _privateConstructorUsedError;
+  set borderColor(int value) => throw _privateConstructorUsedError;
+  double get borderRadius => throw _privateConstructorUsedError;
+  set borderRadius(double value) => throw _privateConstructorUsedError;
+  double get borderWidth => throw _privateConstructorUsedError;
+  set borderWidth(double value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get labelFont => throw _privateConstructorUsedError;
+  set labelFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get parameterLabelFont =>
+      throw _privateConstructorUsedError;
+  set parameterLabelFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  double get sectionSize => throw _privateConstructorUsedError;
+  set sectionSize(double value) => throw _privateConstructorUsedError;
+  double get imageSize => throw _privateConstructorUsedError;
+  set imageSize(double value) => throw _privateConstructorUsedError;
+  double get parameterImageSize => throw _privateConstructorUsedError;
+  set parameterImageSize(double value) => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $GenericDeviceCardWidgetConfigCopyWith<GenericDeviceCardWidgetConfig>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $GenericDeviceCardWidgetConfigCopyWith<$Res> {
+  factory $GenericDeviceCardWidgetConfigCopyWith(
+          GenericDeviceCardWidgetConfig value,
+          $Res Function(GenericDeviceCardWidgetConfig) then) =
+      _$GenericDeviceCardWidgetConfigCopyWithImpl<$Res,
+          GenericDeviceCardWidgetConfig>;
+  @useResult
+  $Res call(
+      {Map<String, dynamic> titleFont,
+      String modelId,
+      int borderColor,
+      double borderRadius,
+      double borderWidth,
+      Map<String, dynamic> labelFont,
+      Map<String, dynamic> parameterLabelFont,
+      double sectionSize,
+      double imageSize,
+      double parameterImageSize});
+}
+
+/// @nodoc
+class _$GenericDeviceCardWidgetConfigCopyWithImpl<$Res,
+        $Val extends GenericDeviceCardWidgetConfig>
+    implements $GenericDeviceCardWidgetConfigCopyWith<$Res> {
+  _$GenericDeviceCardWidgetConfigCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? titleFont = null,
+    Object? modelId = null,
+    Object? borderColor = null,
+    Object? borderRadius = null,
+    Object? borderWidth = null,
+    Object? labelFont = null,
+    Object? parameterLabelFont = null,
+    Object? sectionSize = null,
+    Object? imageSize = null,
+    Object? parameterImageSize = null,
+  }) {
+    return _then(_value.copyWith(
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      modelId: null == modelId
+          ? _value.modelId
+          : modelId // ignore: cast_nullable_to_non_nullable
+              as String,
+      borderColor: null == borderColor
+          ? _value.borderColor
+          : borderColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      borderRadius: null == borderRadius
+          ? _value.borderRadius
+          : borderRadius // ignore: cast_nullable_to_non_nullable
+              as double,
+      borderWidth: null == borderWidth
+          ? _value.borderWidth
+          : borderWidth // ignore: cast_nullable_to_non_nullable
+              as double,
+      labelFont: null == labelFont
+          ? _value.labelFont
+          : labelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      parameterLabelFont: null == parameterLabelFont
+          ? _value.parameterLabelFont
+          : parameterLabelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      sectionSize: null == sectionSize
+          ? _value.sectionSize
+          : sectionSize // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+      parameterImageSize: null == parameterImageSize
+          ? _value.parameterImageSize
+          : parameterImageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$GenericDeviceCardWidgetConfigImplCopyWith<$Res>
+    implements $GenericDeviceCardWidgetConfigCopyWith<$Res> {
+  factory _$$GenericDeviceCardWidgetConfigImplCopyWith(
+          _$GenericDeviceCardWidgetConfigImpl value,
+          $Res Function(_$GenericDeviceCardWidgetConfigImpl) then) =
+      __$$GenericDeviceCardWidgetConfigImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {Map<String, dynamic> titleFont,
+      String modelId,
+      int borderColor,
+      double borderRadius,
+      double borderWidth,
+      Map<String, dynamic> labelFont,
+      Map<String, dynamic> parameterLabelFont,
+      double sectionSize,
+      double imageSize,
+      double parameterImageSize});
+}
+
+/// @nodoc
+class __$$GenericDeviceCardWidgetConfigImplCopyWithImpl<$Res>
+    extends _$GenericDeviceCardWidgetConfigCopyWithImpl<$Res,
+        _$GenericDeviceCardWidgetConfigImpl>
+    implements _$$GenericDeviceCardWidgetConfigImplCopyWith<$Res> {
+  __$$GenericDeviceCardWidgetConfigImplCopyWithImpl(
+      _$GenericDeviceCardWidgetConfigImpl _value,
+      $Res Function(_$GenericDeviceCardWidgetConfigImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? titleFont = null,
+    Object? modelId = null,
+    Object? borderColor = null,
+    Object? borderRadius = null,
+    Object? borderWidth = null,
+    Object? labelFont = null,
+    Object? parameterLabelFont = null,
+    Object? sectionSize = null,
+    Object? imageSize = null,
+    Object? parameterImageSize = null,
+  }) {
+    return _then(_$GenericDeviceCardWidgetConfigImpl(
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      modelId: null == modelId
+          ? _value.modelId
+          : modelId // ignore: cast_nullable_to_non_nullable
+              as String,
+      borderColor: null == borderColor
+          ? _value.borderColor
+          : borderColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      borderRadius: null == borderRadius
+          ? _value.borderRadius
+          : borderRadius // ignore: cast_nullable_to_non_nullable
+              as double,
+      borderWidth: null == borderWidth
+          ? _value.borderWidth
+          : borderWidth // ignore: cast_nullable_to_non_nullable
+              as double,
+      labelFont: null == labelFont
+          ? _value.labelFont
+          : labelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      parameterLabelFont: null == parameterLabelFont
+          ? _value.parameterLabelFont
+          : parameterLabelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      sectionSize: null == sectionSize
+          ? _value.sectionSize
+          : sectionSize // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+      parameterImageSize: null == parameterImageSize
+          ? _value.parameterImageSize
+          : parameterImageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$GenericDeviceCardWidgetConfigImpl
+    extends _GenericDeviceCardWidgetConfig {
+  _$GenericDeviceCardWidgetConfigImpl(
+      {this.titleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 25,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.modelId = '',
+      this.borderColor = 0xFF000000,
+      this.borderRadius = 15,
+      this.borderWidth = 1,
+      this.labelFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 20,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.parameterLabelFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 15,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.sectionSize = 200,
+      this.imageSize = 70,
+      this.parameterImageSize = 40})
+      : super._();
+
+  factory _$GenericDeviceCardWidgetConfigImpl.fromJson(
+          Map<String, dynamic> json) =>
+      _$$GenericDeviceCardWidgetConfigImplFromJson(json);
+
+  @override
+  @JsonKey()
+  Map<String, dynamic> titleFont;
+  @override
+  @JsonKey()
+  String modelId;
+  @override
+  @JsonKey()
+  int borderColor;
+  @override
+  @JsonKey()
+  double borderRadius;
+  @override
+  @JsonKey()
+  double borderWidth;
+  @override
+  @JsonKey()
+  Map<String, dynamic> labelFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> parameterLabelFont;
+  @override
+  @JsonKey()
+  double sectionSize;
+  @override
+  @JsonKey()
+  double imageSize;
+  @override
+  @JsonKey()
+  double parameterImageSize;
+
+  @override
+  String toString() {
+    return 'GenericDeviceCardWidgetConfig(titleFont: $titleFont, modelId: $modelId, borderColor: $borderColor, borderRadius: $borderRadius, borderWidth: $borderWidth, labelFont: $labelFont, parameterLabelFont: $parameterLabelFont, sectionSize: $sectionSize, imageSize: $imageSize, parameterImageSize: $parameterImageSize)';
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$GenericDeviceCardWidgetConfigImplCopyWith<
+          _$GenericDeviceCardWidgetConfigImpl>
+      get copyWith => __$$GenericDeviceCardWidgetConfigImplCopyWithImpl<
+          _$GenericDeviceCardWidgetConfigImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$GenericDeviceCardWidgetConfigImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _GenericDeviceCardWidgetConfig
+    extends GenericDeviceCardWidgetConfig {
+  factory _GenericDeviceCardWidgetConfig(
+      {Map<String, dynamic> titleFont,
+      String modelId,
+      int borderColor,
+      double borderRadius,
+      double borderWidth,
+      Map<String, dynamic> labelFont,
+      Map<String, dynamic> parameterLabelFont,
+      double sectionSize,
+      double imageSize,
+      double parameterImageSize}) = _$GenericDeviceCardWidgetConfigImpl;
+  _GenericDeviceCardWidgetConfig._() : super._();
+
+  factory _GenericDeviceCardWidgetConfig.fromJson(Map<String, dynamic> json) =
+      _$GenericDeviceCardWidgetConfigImpl.fromJson;
+
+  @override
+  Map<String, dynamic> get titleFont;
+  set titleFont(Map<String, dynamic> value);
+  @override
+  String get modelId;
+  set modelId(String value);
+  @override
+  int get borderColor;
+  set borderColor(int value);
+  @override
+  double get borderRadius;
+  set borderRadius(double value);
+  @override
+  double get borderWidth;
+  set borderWidth(double value);
+  @override
+  Map<String, dynamic> get labelFont;
+  set labelFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get parameterLabelFont;
+  set parameterLabelFont(Map<String, dynamic> value);
+  @override
+  double get sectionSize;
+  set sectionSize(double value);
+  @override
+  double get imageSize;
+  set imageSize(double value);
+  @override
+  double get parameterImageSize;
+  set parameterImageSize(double value);
+  @override
+  @JsonKey(ignore: true)
+  _$$GenericDeviceCardWidgetConfigImplCopyWith<
+          _$GenericDeviceCardWidgetConfigImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
