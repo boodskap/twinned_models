623224709a608e29a99f9c94dcb976edcd1af7f8
DhanuElavarasan1302 <nreldhanu1713@gmail.com>
2024-10-09 10:44:12 +0530

    Generic Up Down Semi Circle Widget added

---
+++ lib/generic_up_down_semi_circle/generic_up_down_semi_circle.freezed.dart
@@ -0,0 +1,448 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'generic_up_down_semi_circle.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+GenericUpDownSemiCircleWidgetConfig
+    _$GenericUpDownSemiCircleWidgetConfigFromJson(Map<String, dynamic> json) {
+  return _GenericUpDownSemiCircleWidgetConfig.fromJson(json);
+}
+
+/// @nodoc
+mixin _$GenericUpDownSemiCircleWidgetConfig {
+  String get title => throw _privateConstructorUsedError;
+  set title(String value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get titleFont => throw _privateConstructorUsedError;
+  set titleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  String get subTitle => throw _privateConstructorUsedError;
+  set subTitle(String value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get subTitleFont => throw _privateConstructorUsedError;
+  set subTitleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  String get deviceId => throw _privateConstructorUsedError;
+  set deviceId(String value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get prefixFont => throw _privateConstructorUsedError;
+  set prefixFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get suffixFont => throw _privateConstructorUsedError;
+  set suffixFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get valueFont => throw _privateConstructorUsedError;
+  set valueFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  int get upSemiCircleBGColor => throw _privateConstructorUsedError;
+  set upSemiCircleBGColor(int value) => throw _privateConstructorUsedError;
+  int get downSemiCircleBGColor => throw _privateConstructorUsedError;
+  set downSemiCircleBGColor(int value) => throw _privateConstructorUsedError;
+  double get horizontalSpacing => throw _privateConstructorUsedError;
+  set horizontalSpacing(double value) => throw _privateConstructorUsedError;
+  double get imageSize => throw _privateConstructorUsedError;
+  set imageSize(double value) => throw _privateConstructorUsedError;
+
+  /// Serializes this GenericUpDownSemiCircleWidgetConfig to a JSON map.
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  /// Create a copy of GenericUpDownSemiCircleWidgetConfig
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $GenericUpDownSemiCircleWidgetConfigCopyWith<
+          GenericUpDownSemiCircleWidgetConfig>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $GenericUpDownSemiCircleWidgetConfigCopyWith<$Res> {
+  factory $GenericUpDownSemiCircleWidgetConfigCopyWith(
+          GenericUpDownSemiCircleWidgetConfig value,
+          $Res Function(GenericUpDownSemiCircleWidgetConfig) then) =
+      _$GenericUpDownSemiCircleWidgetConfigCopyWithImpl<$Res,
+          GenericUpDownSemiCircleWidgetConfig>;
+  @useResult
+  $Res call(
+      {String title,
+      Map<String, dynamic> titleFont,
+      String subTitle,
+      Map<String, dynamic> subTitleFont,
+      String deviceId,
+      Map<String, dynamic> prefixFont,
+      Map<String, dynamic> suffixFont,
+      Map<String, dynamic> valueFont,
+      int upSemiCircleBGColor,
+      int downSemiCircleBGColor,
+      double horizontalSpacing,
+      double imageSize});
+}
+
+/// @nodoc
+class _$GenericUpDownSemiCircleWidgetConfigCopyWithImpl<$Res,
+        $Val extends GenericUpDownSemiCircleWidgetConfig>
+    implements $GenericUpDownSemiCircleWidgetConfigCopyWith<$Res> {
+  _$GenericUpDownSemiCircleWidgetConfigCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of GenericUpDownSemiCircleWidgetConfig
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? titleFont = null,
+    Object? subTitle = null,
+    Object? subTitleFont = null,
+    Object? deviceId = null,
+    Object? prefixFont = null,
+    Object? suffixFont = null,
+    Object? valueFont = null,
+    Object? upSemiCircleBGColor = null,
+    Object? downSemiCircleBGColor = null,
+    Object? horizontalSpacing = null,
+    Object? imageSize = null,
+  }) {
+    return _then(_value.copyWith(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      subTitle: null == subTitle
+          ? _value.subTitle
+          : subTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      subTitleFont: null == subTitleFont
+          ? _value.subTitleFont
+          : subTitleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      prefixFont: null == prefixFont
+          ? _value.prefixFont
+          : prefixFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      suffixFont: null == suffixFont
+          ? _value.suffixFont
+          : suffixFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      upSemiCircleBGColor: null == upSemiCircleBGColor
+          ? _value.upSemiCircleBGColor
+          : upSemiCircleBGColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      downSemiCircleBGColor: null == downSemiCircleBGColor
+          ? _value.downSemiCircleBGColor
+          : downSemiCircleBGColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      horizontalSpacing: null == horizontalSpacing
+          ? _value.horizontalSpacing
+          : horizontalSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$GenericUpDownSemiCircleWidgetConfigImplCopyWith<$Res>
+    implements $GenericUpDownSemiCircleWidgetConfigCopyWith<$Res> {
+  factory _$$GenericUpDownSemiCircleWidgetConfigImplCopyWith(
+          _$GenericUpDownSemiCircleWidgetConfigImpl value,
+          $Res Function(_$GenericUpDownSemiCircleWidgetConfigImpl) then) =
+      __$$GenericUpDownSemiCircleWidgetConfigImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String title,
+      Map<String, dynamic> titleFont,
+      String subTitle,
+      Map<String, dynamic> subTitleFont,
+      String deviceId,
+      Map<String, dynamic> prefixFont,
+      Map<String, dynamic> suffixFont,
+      Map<String, dynamic> valueFont,
+      int upSemiCircleBGColor,
+      int downSemiCircleBGColor,
+      double horizontalSpacing,
+      double imageSize});
+}
+
+/// @nodoc
+class __$$GenericUpDownSemiCircleWidgetConfigImplCopyWithImpl<$Res>
+    extends _$GenericUpDownSemiCircleWidgetConfigCopyWithImpl<$Res,
+        _$GenericUpDownSemiCircleWidgetConfigImpl>
+    implements _$$GenericUpDownSemiCircleWidgetConfigImplCopyWith<$Res> {
+  __$$GenericUpDownSemiCircleWidgetConfigImplCopyWithImpl(
+      _$GenericUpDownSemiCircleWidgetConfigImpl _value,
+      $Res Function(_$GenericUpDownSemiCircleWidgetConfigImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of GenericUpDownSemiCircleWidgetConfig
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? titleFont = null,
+    Object? subTitle = null,
+    Object? subTitleFont = null,
+    Object? deviceId = null,
+    Object? prefixFont = null,
+    Object? suffixFont = null,
+    Object? valueFont = null,
+    Object? upSemiCircleBGColor = null,
+    Object? downSemiCircleBGColor = null,
+    Object? horizontalSpacing = null,
+    Object? imageSize = null,
+  }) {
+    return _then(_$GenericUpDownSemiCircleWidgetConfigImpl(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      subTitle: null == subTitle
+          ? _value.subTitle
+          : subTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      subTitleFont: null == subTitleFont
+          ? _value.subTitleFont
+          : subTitleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      prefixFont: null == prefixFont
+          ? _value.prefixFont
+          : prefixFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      suffixFont: null == suffixFont
+          ? _value.suffixFont
+          : suffixFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      upSemiCircleBGColor: null == upSemiCircleBGColor
+          ? _value.upSemiCircleBGColor
+          : upSemiCircleBGColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      downSemiCircleBGColor: null == downSemiCircleBGColor
+          ? _value.downSemiCircleBGColor
+          : downSemiCircleBGColor // ignore: cast_nullable_to_non_nullable
+              as int,
+      horizontalSpacing: null == horizontalSpacing
+          ? _value.horizontalSpacing
+          : horizontalSpacing // ignore: cast_nullable_to_non_nullable
+              as double,
+      imageSize: null == imageSize
+          ? _value.imageSize
+          : imageSize // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$GenericUpDownSemiCircleWidgetConfigImpl
+    extends _GenericUpDownSemiCircleWidgetConfig {
+  _$GenericUpDownSemiCircleWidgetConfigImpl(
+      {this.title = 'Generic Up Down Semi Circle',
+      this.titleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 25,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.subTitle = 'Check Your Device Data By Using Semi Circle Shape!',
+      this.subTitleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 20,
+        'fontColor': 0xFF3B444B,
+        'fontBold': false
+      },
+      this.deviceId = '',
+      this.prefixFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 12,
+        'fontColor': 0xFF000000,
+        'fontBold': false
+      },
+      this.suffixFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 12,
+        'fontColor': 0xFF000000,
+        'fontBold': false
+      },
+      this.valueFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 14,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.upSemiCircleBGColor = 0XFF7CDAB8,
+      this.downSemiCircleBGColor = 0XFFf79cA1,
+      this.horizontalSpacing = 5,
+      this.imageSize = 25})
+      : super._();
+
+  factory _$GenericUpDownSemiCircleWidgetConfigImpl.fromJson(
+          Map<String, dynamic> json) =>
+      _$$GenericUpDownSemiCircleWidgetConfigImplFromJson(json);
+
+  @override
+  @JsonKey()
+  String title;
+  @override
+  @JsonKey()
+  Map<String, dynamic> titleFont;
+  @override
+  @JsonKey()
+  String subTitle;
+  @override
+  @JsonKey()
+  Map<String, dynamic> subTitleFont;
+  @override
+  @JsonKey()
+  String deviceId;
+  @override
+  @JsonKey()
+  Map<String, dynamic> prefixFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> suffixFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> valueFont;
+  @override
+  @JsonKey()
+  int upSemiCircleBGColor;
+  @override
+  @JsonKey()
+  int downSemiCircleBGColor;
+  @override
+  @JsonKey()
+  double horizontalSpacing;
+  @override
+  @JsonKey()
+  double imageSize;
+
+  @override
+  String toString() {
+    return 'GenericUpDownSemiCircleWidgetConfig(title: $title, titleFont: $titleFont, subTitle: $subTitle, subTitleFont: $subTitleFont, deviceId: $deviceId, prefixFont: $prefixFont, suffixFont: $suffixFont, valueFont: $valueFont, upSemiCircleBGColor: $upSemiCircleBGColor, downSemiCircleBGColor: $downSemiCircleBGColor, horizontalSpacing: $horizontalSpacing, imageSize: $imageSize)';
+  }
+
+  /// Create a copy of GenericUpDownSemiCircleWidgetConfig
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$GenericUpDownSemiCircleWidgetConfigImplCopyWith<
+          _$GenericUpDownSemiCircleWidgetConfigImpl>
+      get copyWith => __$$GenericUpDownSemiCircleWidgetConfigImplCopyWithImpl<
+          _$GenericUpDownSemiCircleWidgetConfigImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$GenericUpDownSemiCircleWidgetConfigImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _GenericUpDownSemiCircleWidgetConfig
+    extends GenericUpDownSemiCircleWidgetConfig {
+  factory _GenericUpDownSemiCircleWidgetConfig(
+      {String title,
+      Map<String, dynamic> titleFont,
+      String subTitle,
+      Map<String, dynamic> subTitleFont,
+      String deviceId,
+      Map<String, dynamic> prefixFont,
+      Map<String, dynamic> suffixFont,
+      Map<String, dynamic> valueFont,
+      int upSemiCircleBGColor,
+      int downSemiCircleBGColor,
+      double horizontalSpacing,
+      double imageSize}) = _$GenericUpDownSemiCircleWidgetConfigImpl;
+  _GenericUpDownSemiCircleWidgetConfig._() : super._();
+
+  factory _GenericUpDownSemiCircleWidgetConfig.fromJson(
+          Map<String, dynamic> json) =
+      _$GenericUpDownSemiCircleWidgetConfigImpl.fromJson;
+
+  @override
+  String get title;
+  set title(String value);
+  @override
+  Map<String, dynamic> get titleFont;
+  set titleFont(Map<String, dynamic> value);
+  @override
+  String get subTitle;
+  set subTitle(String value);
+  @override
+  Map<String, dynamic> get subTitleFont;
+  set subTitleFont(Map<String, dynamic> value);
+  @override
+  String get deviceId;
+  set deviceId(String value);
+  @override
+  Map<String, dynamic> get prefixFont;
+  set prefixFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get suffixFont;
+  set suffixFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get valueFont;
+  set valueFont(Map<String, dynamic> value);
+  @override
+  int get upSemiCircleBGColor;
+  set upSemiCircleBGColor(int value);
+  @override
+  int get downSemiCircleBGColor;
+  set downSemiCircleBGColor(int value);
+  @override
+  double get horizontalSpacing;
+  set horizontalSpacing(double value);
+  @override
+  double get imageSize;
+  set imageSize(double value);
+
+  /// Create a copy of GenericUpDownSemiCircleWidgetConfig
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$GenericUpDownSemiCircleWidgetConfigImplCopyWith<
+          _$GenericUpDownSemiCircleWidgetConfigImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
