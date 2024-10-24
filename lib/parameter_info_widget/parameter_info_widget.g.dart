dc3857e2cc64518003321992c24c7914d50b6e98
Thirumalvalavan <thirumalvalavan99@gmail.com>
2024-10-24 13:25:05 +0530

    paramete info widget, value

---
+++ lib/parameter_info_widget/parameter_info_widget.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'parameter_info_widget.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ParameterInfoWidgetConfigImpl _$$ParameterInfoWidgetConfigImplFromJson(
+        Map<String, dynamic> json) =>
+    _$ParameterInfoWidgetConfigImpl(
+      deviceId: json['deviceId'] as String? ?? '',
+      field: json['field'] as String? ?? '',
+      title: json['title'] as String? ?? '',
+      hintText: json['hintText'] as String? ?? '',
+      iconColor: (json['iconColor'] as num?)?.toInt() ?? 0XFF03A9F4,
+      titleFont: json['titleFont'] as Map<String, dynamic>? ??
+          const {
+            'fontFamily': 'Open Sans',
+            'fontSize': 14,
+            'fontColor': 0xFF000000,
+            'fontBold': true
+          },
+      valueFont: json['valueFont'] as Map<String, dynamic>? ??
+          const {
+            'fontFamily': 'Open Sans',
+            'fontSize': 14,
+            'fontColor': 0xDD000000,
+            'fontBold': true
+          },
+      subLabelFont: json['subLabelFont'] as Map<String, dynamic>? ??
+          const {
+            'fontFamily': 'Open Sans',
+            'fontSize': 12,
+            'fontColor': 0x8A000000,
+            'fontBold': true
+          },
+    );
+
+Map<String, dynamic> _$$ParameterInfoWidgetConfigImplToJson(
+        _$ParameterInfoWidgetConfigImpl instance) =>
+    <String, dynamic>{
+      'deviceId': instance.deviceId,
+      'field': instance.field,
+      'title': instance.title,
+      'hintText': instance.hintText,
+      'iconColor': instance.iconColor,
+      'titleFont': instance.titleFont,
+      'valueFont': instance.valueFont,
+      'subLabelFont': instance.subLabelFont,
+    };
---
+++ lib/parameter_value_widget/parameter_value_widget.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'parameter_value_widget.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ParameterValueWidgetConfigImpl _$$ParameterValueWidgetConfigImplFromJson(
+        Map<String, dynamic> json) =>
+    _$ParameterValueWidgetConfigImpl(
+      deviceId: json['deviceId'] as String? ?? '',
+      field: json['field'] as String? ?? '',
+      title: json['title'] as String? ?? '',
+      iconColor: (json['iconColor'] as num?)?.toInt() ?? 0XFF03A9F4,
+      titleFont: json['titleFont'] as Map<String, dynamic>? ??
+          const {
+            'fontFamily': 'Open Sans',
+            'fontSize': 14,
+            'fontColor': 0xFF000000,
+            'fontBold': true
+          },
+      valueFont: json['valueFont'] as Map<String, dynamic>? ??
+          const {
+            'fontFamily': 'Open Sans',
+            'fontSize': 14,
+            'fontColor': 0xDD000000,
+            'fontBold': true
+          },
+      subLabelFont: json['subLabelFont'] as Map<String, dynamic>? ??
+          const {
+            'fontFamily': 'Open Sans',
+            'fontSize': 12,
+            'fontColor': 0x8A000000,
+            'fontBold': true
+          },
+    );
+
+Map<String, dynamic> _$$ParameterValueWidgetConfigImplToJson(
+        _$ParameterValueWidgetConfigImpl instance) =>
+    <String, dynamic>{
+      'deviceId': instance.deviceId,
+      'field': instance.field,
+      'title': instance.title,
+      'iconColor': instance.iconColor,
+      'titleFont': instance.titleFont,
+      'valueFont': instance.valueFont,
+      'subLabelFont': instance.subLabelFont,
+    };
