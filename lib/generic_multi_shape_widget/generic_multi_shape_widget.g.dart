c7dacca417f611895a1092f42cb507186eb200e0
unknown <prithiviraina1998@gmail.com>
2024-10-28 19:24:13 +0530

    Generic Multi Shape Widget Error fixed

--- lib/generic_multi_shape_widget/generic_multi_shape_widget.g.dart
+++ lib/generic_multi_shape_widget/generic_multi_shape_widget.g.dart
@@ -6,53 +6,54 @@ part of 'generic_multi_shape_widget.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$GenericMultiShapeWidgetImpl _$$GenericMultiShapeWidgetImplFromJson(
-        Map<String, dynamic> json) =>
-    _$GenericMultiShapeWidgetImpl(
-      title: json['title'] as String? ?? 'Generic Multi Shape Widget',
-      titleFont: json['titleFont'] as Map<String, dynamic>? ??
-          const {
-            'fontFamily': 'Open Sans',
-            'fontSize': 25,
-            'fontColor': 0xFF000000,
-            'fontBold': true
-          },
-      subTitle: json['subTitle'] as String? ?? 'Check My Device Data here!',
-      subTitleFont: json['subTitleFont'] as Map<String, dynamic>? ??
-          const {
-            'fontFamily': 'Open Sans',
-            'fontSize': 20,
-            'fontColor': 0xFF3B444B,
-            'fontBold': false
-          },
-      deviceId: json['deviceId'] as String? ?? '',
-      prefixFont: json['prefixFont'] as Map<String, dynamic>? ??
-          const {
-            'fontFamily': 'Open Sans',
-            'fontSize': 12,
-            'fontColor': 0xFF000000,
-            'fontBold': false
-          },
-      suffixFont: json['suffixFont'] as Map<String, dynamic>? ??
-          const {
-            'fontFamily': 'Open Sans',
-            'fontSize': 12,
-            'fontColor': 0xFF000000,
-            'fontBold': false
-          },
-      valueFont: json['valueFont'] as Map<String, dynamic>? ??
-          const {
-            'fontFamily': 'Open Sans',
-            'fontSize': 14,
-            'fontColor': 0xFF000000,
-            'fontBold': true
-          },
-      horizontalSpacing: (json['horizontalSpacing'] as num?)?.toDouble() ?? 5,
-      imageSize: (json['imageSize'] as num?)?.toDouble() ?? 25,
-    );
+_$GenericMultiShapeWidgetConfigImpl
+    _$$GenericMultiShapeWidgetConfigImplFromJson(Map<String, dynamic> json) =>
+        _$GenericMultiShapeWidgetConfigImpl(
+          title: json['title'] as String? ?? 'Generic Multi Shape Widget',
+          titleFont: json['titleFont'] as Map<String, dynamic>? ??
+              const {
+                'fontFamily': 'Open Sans',
+                'fontSize': 25,
+                'fontColor': 0xFF000000,
+                'fontBold': true
+              },
+          subTitle: json['subTitle'] as String? ?? 'Check My Device Data here!',
+          subTitleFont: json['subTitleFont'] as Map<String, dynamic>? ??
+              const {
+                'fontFamily': 'Open Sans',
+                'fontSize': 20,
+                'fontColor': 0xFF3B444B,
+                'fontBold': false
+              },
+          deviceId: json['deviceId'] as String? ?? '',
+          prefixFont: json['prefixFont'] as Map<String, dynamic>? ??
+              const {
+                'fontFamily': 'Open Sans',
+                'fontSize': 12,
+                'fontColor': 0xFF000000,
+                'fontBold': false
+              },
+          suffixFont: json['suffixFont'] as Map<String, dynamic>? ??
+              const {
+                'fontFamily': 'Open Sans',
+                'fontSize': 12,
+                'fontColor': 0xFF000000,
+                'fontBold': false
+              },
+          valueFont: json['valueFont'] as Map<String, dynamic>? ??
+              const {
+                'fontFamily': 'Open Sans',
+                'fontSize': 14,
+                'fontColor': 0xFF000000,
+                'fontBold': true
+              },
+          horizontalSpacing:
+              (json['horizontalSpacing'] as num?)?.toDouble() ?? 5,
+          imageSize: (json['imageSize'] as num?)?.toDouble() ?? 25,
+        );
 
-Map<String, dynamic> _$$GenericMultiShapeWidgetImplToJson(
-        _$GenericMultiShapeWidgetImpl instance) =>
+Map<String, dynamic> _$$GenericMultiShapeWidgetConfigImplToJson(
+        _$GenericMultiShapeWidgetConfigImpl instance) =>
     <String, dynamic>{
       'title': instance.title,
       'titleFont': instance.titleFont,
