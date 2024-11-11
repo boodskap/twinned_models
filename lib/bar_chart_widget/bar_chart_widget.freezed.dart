c60da92cbcbbdbdf29eae05a7e9f47040fc582fc
Thirumalvalavan <thirumalvalavan99@gmail.com>
2024-11-11 15:20:35 +0530

    Bar Chart Widget config added

---
+++ lib/bar_chart_widget/bar_chart_widget.freezed.dart
@@ -0,0 +1,314 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'bar_chart_widget.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+BarChartWidgetConfig _$BarChartWidgetConfigFromJson(Map<String, dynamic> json) {
+  return _BarChartWidgetConfig.fromJson(json);
+}
+
+/// @nodoc
+mixin _$BarChartWidgetConfig {
+  String get title => throw _privateConstructorUsedError;
+  set title(String value) => throw _privateConstructorUsedError;
+  String get deviceId => throw _privateConstructorUsedError;
+  set deviceId(String value) => throw _privateConstructorUsedError;
+  String get field => throw _privateConstructorUsedError;
+  set field(String value) => throw _privateConstructorUsedError;
+  double get barWidth => throw _privateConstructorUsedError;
+  set barWidth(double value) => throw _privateConstructorUsedError;
+  double get barRadius => throw _privateConstructorUsedError;
+  set barRadius(double value) => throw _privateConstructorUsedError;
+  dynamic get barColor => throw _privateConstructorUsedError;
+  set barColor(dynamic value) => throw _privateConstructorUsedError;
+  Map<String, dynamic> get titleFont => throw _privateConstructorUsedError;
+  set titleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get labelFont => throw _privateConstructorUsedError;
+  set labelFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $BarChartWidgetConfigCopyWith<BarChartWidgetConfig> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BarChartWidgetConfigCopyWith<$Res> {
+  factory $BarChartWidgetConfigCopyWith(BarChartWidgetConfig value,
+          $Res Function(BarChartWidgetConfig) then) =
+      _$BarChartWidgetConfigCopyWithImpl<$Res, BarChartWidgetConfig>;
+  @useResult
+  $Res call(
+      {String title,
+      String deviceId,
+      String field,
+      double barWidth,
+      double barRadius,
+      dynamic barColor,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> labelFont});
+}
+
+/// @nodoc
+class _$BarChartWidgetConfigCopyWithImpl<$Res,
+        $Val extends BarChartWidgetConfig>
+    implements $BarChartWidgetConfigCopyWith<$Res> {
+  _$BarChartWidgetConfigCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? deviceId = null,
+    Object? field = null,
+    Object? barWidth = null,
+    Object? barRadius = null,
+    Object? barColor = freezed,
+    Object? titleFont = null,
+    Object? labelFont = null,
+  }) {
+    return _then(_value.copyWith(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      field: null == field
+          ? _value.field
+          : field // ignore: cast_nullable_to_non_nullable
+              as String,
+      barWidth: null == barWidth
+          ? _value.barWidth
+          : barWidth // ignore: cast_nullable_to_non_nullable
+              as double,
+      barRadius: null == barRadius
+          ? _value.barRadius
+          : barRadius // ignore: cast_nullable_to_non_nullable
+              as double,
+      barColor: freezed == barColor
+          ? _value.barColor
+          : barColor // ignore: cast_nullable_to_non_nullable
+              as dynamic,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      labelFont: null == labelFont
+          ? _value.labelFont
+          : labelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$BarChartWidgetConfigImplCopyWith<$Res>
+    implements $BarChartWidgetConfigCopyWith<$Res> {
+  factory _$$BarChartWidgetConfigImplCopyWith(_$BarChartWidgetConfigImpl value,
+          $Res Function(_$BarChartWidgetConfigImpl) then) =
+      __$$BarChartWidgetConfigImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String title,
+      String deviceId,
+      String field,
+      double barWidth,
+      double barRadius,
+      dynamic barColor,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> labelFont});
+}
+
+/// @nodoc
+class __$$BarChartWidgetConfigImplCopyWithImpl<$Res>
+    extends _$BarChartWidgetConfigCopyWithImpl<$Res, _$BarChartWidgetConfigImpl>
+    implements _$$BarChartWidgetConfigImplCopyWith<$Res> {
+  __$$BarChartWidgetConfigImplCopyWithImpl(_$BarChartWidgetConfigImpl _value,
+      $Res Function(_$BarChartWidgetConfigImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? deviceId = null,
+    Object? field = null,
+    Object? barWidth = null,
+    Object? barRadius = null,
+    Object? barColor = freezed,
+    Object? titleFont = null,
+    Object? labelFont = null,
+  }) {
+    return _then(_$BarChartWidgetConfigImpl(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      field: null == field
+          ? _value.field
+          : field // ignore: cast_nullable_to_non_nullable
+              as String,
+      barWidth: null == barWidth
+          ? _value.barWidth
+          : barWidth // ignore: cast_nullable_to_non_nullable
+              as double,
+      barRadius: null == barRadius
+          ? _value.barRadius
+          : barRadius // ignore: cast_nullable_to_non_nullable
+              as double,
+      barColor: freezed == barColor ? _value.barColor! : barColor,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      labelFont: null == labelFont
+          ? _value.labelFont
+          : labelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$BarChartWidgetConfigImpl extends _BarChartWidgetConfig {
+  _$BarChartWidgetConfigImpl(
+      {this.title = '',
+      this.deviceId = '',
+      this.field = '',
+      this.barWidth = 0.15,
+      this.barRadius = 8,
+      this.barColor = 0XFFF8F8FF,
+      this.titleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 14,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.labelFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 10,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      }})
+      : super._();
+
+  factory _$BarChartWidgetConfigImpl.fromJson(Map<String, dynamic> json) =>
+      _$$BarChartWidgetConfigImplFromJson(json);
+
+  @override
+  @JsonKey()
+  String title;
+  @override
+  @JsonKey()
+  String deviceId;
+  @override
+  @JsonKey()
+  String field;
+  @override
+  @JsonKey()
+  double barWidth;
+  @override
+  @JsonKey()
+  double barRadius;
+  @override
+  @JsonKey()
+  dynamic barColor;
+  @override
+  @JsonKey()
+  Map<String, dynamic> titleFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> labelFont;
+
+  @override
+  String toString() {
+    return 'BarChartWidgetConfig(title: $title, deviceId: $deviceId, field: $field, barWidth: $barWidth, barRadius: $barRadius, barColor: $barColor, titleFont: $titleFont, labelFont: $labelFont)';
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$BarChartWidgetConfigImplCopyWith<_$BarChartWidgetConfigImpl>
+      get copyWith =>
+          __$$BarChartWidgetConfigImplCopyWithImpl<_$BarChartWidgetConfigImpl>(
+              this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$BarChartWidgetConfigImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _BarChartWidgetConfig extends BarChartWidgetConfig {
+  factory _BarChartWidgetConfig(
+      {String title,
+      String deviceId,
+      String field,
+      double barWidth,
+      double barRadius,
+      dynamic barColor,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> labelFont}) = _$BarChartWidgetConfigImpl;
+  _BarChartWidgetConfig._() : super._();
+
+  factory _BarChartWidgetConfig.fromJson(Map<String, dynamic> json) =
+      _$BarChartWidgetConfigImpl.fromJson;
+
+  @override
+  String get title;
+  set title(String value);
+  @override
+  String get deviceId;
+  set deviceId(String value);
+  @override
+  String get field;
+  set field(String value);
+  @override
+  double get barWidth;
+  set barWidth(double value);
+  @override
+  double get barRadius;
+  set barRadius(double value);
+  @override
+  dynamic get barColor;
+  set barColor(dynamic value);
+  @override
+  Map<String, dynamic> get titleFont;
+  set titleFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get labelFont;
+  set labelFont(Map<String, dynamic> value);
+  @override
+  @JsonKey(ignore: true)
+  _$$BarChartWidgetConfigImplCopyWith<_$BarChartWidgetConfigImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
