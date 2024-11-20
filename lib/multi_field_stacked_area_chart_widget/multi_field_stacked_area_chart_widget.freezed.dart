1f63aeb1515ae0c37de3fae53cc8ffed6ba89b0c
Thirumalvalavan <thirumalvalavan99@gmail.com>
2024-11-20 16:22:48 +0530

    Multi field Stacked

---
+++ lib/multi_field_stacked_area_chart_widget/multi_field_stacked_area_chart_widget.freezed.dart
@@ -0,0 +1,480 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'multi_field_stacked_area_chart_widget.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+MultiFieldStackedAreaChartConfig _$MultiFieldStackedAreaChartConfigFromJson(
+    Map<String, dynamic> json) {
+  return _MultiFieldStackedAreaChartConfig.fromJson(json);
+}
+
+/// @nodoc
+mixin _$MultiFieldStackedAreaChartConfig {
+  String get title => throw _privateConstructorUsedError;
+  set title(String value) => throw _privateConstructorUsedError;
+  String get deviceId => throw _privateConstructorUsedError;
+  set deviceId(String value) => throw _privateConstructorUsedError;
+  List<String> get fields => throw _privateConstructorUsedError;
+  set fields(List<String> value) => throw _privateConstructorUsedError;
+  List<int> get chartColors => throw _privateConstructorUsedError;
+  set chartColors(List<int> value) => throw _privateConstructorUsedError;
+  bool get legendVisibility => throw _privateConstructorUsedError;
+  set legendVisibility(bool value) => throw _privateConstructorUsedError;
+  bool get enableTooltip => throw _privateConstructorUsedError;
+  set enableTooltip(bool value) => throw _privateConstructorUsedError;
+  bool get enableDataLabel => throw _privateConstructorUsedError;
+  set enableDataLabel(bool value) => throw _privateConstructorUsedError;
+  bool get enableMarkerPoint => throw _privateConstructorUsedError;
+  set enableMarkerPoint(bool value) => throw _privateConstructorUsedError;
+  LegendPosition get legendPosition => throw _privateConstructorUsedError;
+  set legendPosition(LegendPosition value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get titleFont => throw _privateConstructorUsedError;
+  set titleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get labelFont => throw _privateConstructorUsedError;
+  set labelFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get subTitleFont => throw _privateConstructorUsedError;
+  set subTitleFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get valueFont => throw _privateConstructorUsedError;
+  set valueFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+  Map<String, dynamic> get tooltipFont => throw _privateConstructorUsedError;
+  set tooltipFont(Map<String, dynamic> value) =>
+      throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $MultiFieldStackedAreaChartConfigCopyWith<MultiFieldStackedAreaChartConfig>
+      get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MultiFieldStackedAreaChartConfigCopyWith<$Res> {
+  factory $MultiFieldStackedAreaChartConfigCopyWith(
+          MultiFieldStackedAreaChartConfig value,
+          $Res Function(MultiFieldStackedAreaChartConfig) then) =
+      _$MultiFieldStackedAreaChartConfigCopyWithImpl<$Res,
+          MultiFieldStackedAreaChartConfig>;
+  @useResult
+  $Res call(
+      {String title,
+      String deviceId,
+      List<String> fields,
+      List<int> chartColors,
+      bool legendVisibility,
+      bool enableTooltip,
+      bool enableDataLabel,
+      bool enableMarkerPoint,
+      LegendPosition legendPosition,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> labelFont,
+      Map<String, dynamic> subTitleFont,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> tooltipFont});
+}
+
+/// @nodoc
+class _$MultiFieldStackedAreaChartConfigCopyWithImpl<$Res,
+        $Val extends MultiFieldStackedAreaChartConfig>
+    implements $MultiFieldStackedAreaChartConfigCopyWith<$Res> {
+  _$MultiFieldStackedAreaChartConfigCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? deviceId = null,
+    Object? fields = null,
+    Object? chartColors = null,
+    Object? legendVisibility = null,
+    Object? enableTooltip = null,
+    Object? enableDataLabel = null,
+    Object? enableMarkerPoint = null,
+    Object? legendPosition = null,
+    Object? titleFont = null,
+    Object? labelFont = null,
+    Object? subTitleFont = null,
+    Object? valueFont = null,
+    Object? tooltipFont = null,
+  }) {
+    return _then(_value.copyWith(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      fields: null == fields
+          ? _value.fields
+          : fields // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      chartColors: null == chartColors
+          ? _value.chartColors
+          : chartColors // ignore: cast_nullable_to_non_nullable
+              as List<int>,
+      legendVisibility: null == legendVisibility
+          ? _value.legendVisibility
+          : legendVisibility // ignore: cast_nullable_to_non_nullable
+              as bool,
+      enableTooltip: null == enableTooltip
+          ? _value.enableTooltip
+          : enableTooltip // ignore: cast_nullable_to_non_nullable
+              as bool,
+      enableDataLabel: null == enableDataLabel
+          ? _value.enableDataLabel
+          : enableDataLabel // ignore: cast_nullable_to_non_nullable
+              as bool,
+      enableMarkerPoint: null == enableMarkerPoint
+          ? _value.enableMarkerPoint
+          : enableMarkerPoint // ignore: cast_nullable_to_non_nullable
+              as bool,
+      legendPosition: null == legendPosition
+          ? _value.legendPosition
+          : legendPosition // ignore: cast_nullable_to_non_nullable
+              as LegendPosition,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      labelFont: null == labelFont
+          ? _value.labelFont
+          : labelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      subTitleFont: null == subTitleFont
+          ? _value.subTitleFont
+          : subTitleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      tooltipFont: null == tooltipFont
+          ? _value.tooltipFont
+          : tooltipFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$MultiFieldStackedAreaChartConfigImplCopyWith<$Res>
+    implements $MultiFieldStackedAreaChartConfigCopyWith<$Res> {
+  factory _$$MultiFieldStackedAreaChartConfigImplCopyWith(
+          _$MultiFieldStackedAreaChartConfigImpl value,
+          $Res Function(_$MultiFieldStackedAreaChartConfigImpl) then) =
+      __$$MultiFieldStackedAreaChartConfigImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String title,
+      String deviceId,
+      List<String> fields,
+      List<int> chartColors,
+      bool legendVisibility,
+      bool enableTooltip,
+      bool enableDataLabel,
+      bool enableMarkerPoint,
+      LegendPosition legendPosition,
+      Map<String, dynamic> titleFont,
+      Map<String, dynamic> labelFont,
+      Map<String, dynamic> subTitleFont,
+      Map<String, dynamic> valueFont,
+      Map<String, dynamic> tooltipFont});
+}
+
+/// @nodoc
+class __$$MultiFieldStackedAreaChartConfigImplCopyWithImpl<$Res>
+    extends _$MultiFieldStackedAreaChartConfigCopyWithImpl<$Res,
+        _$MultiFieldStackedAreaChartConfigImpl>
+    implements _$$MultiFieldStackedAreaChartConfigImplCopyWith<$Res> {
+  __$$MultiFieldStackedAreaChartConfigImplCopyWithImpl(
+      _$MultiFieldStackedAreaChartConfigImpl _value,
+      $Res Function(_$MultiFieldStackedAreaChartConfigImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? deviceId = null,
+    Object? fields = null,
+    Object? chartColors = null,
+    Object? legendVisibility = null,
+    Object? enableTooltip = null,
+    Object? enableDataLabel = null,
+    Object? enableMarkerPoint = null,
+    Object? legendPosition = null,
+    Object? titleFont = null,
+    Object? labelFont = null,
+    Object? subTitleFont = null,
+    Object? valueFont = null,
+    Object? tooltipFont = null,
+  }) {
+    return _then(_$MultiFieldStackedAreaChartConfigImpl(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      deviceId: null == deviceId
+          ? _value.deviceId
+          : deviceId // ignore: cast_nullable_to_non_nullable
+              as String,
+      fields: null == fields
+          ? _value.fields
+          : fields // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      chartColors: null == chartColors
+          ? _value.chartColors
+          : chartColors // ignore: cast_nullable_to_non_nullable
+              as List<int>,
+      legendVisibility: null == legendVisibility
+          ? _value.legendVisibility
+          : legendVisibility // ignore: cast_nullable_to_non_nullable
+              as bool,
+      enableTooltip: null == enableTooltip
+          ? _value.enableTooltip
+          : enableTooltip // ignore: cast_nullable_to_non_nullable
+              as bool,
+      enableDataLabel: null == enableDataLabel
+          ? _value.enableDataLabel
+          : enableDataLabel // ignore: cast_nullable_to_non_nullable
+              as bool,
+      enableMarkerPoint: null == enableMarkerPoint
+          ? _value.enableMarkerPoint
+          : enableMarkerPoint // ignore: cast_nullable_to_non_nullable
+              as bool,
+      legendPosition: null == legendPosition
+          ? _value.legendPosition
+          : legendPosition // ignore: cast_nullable_to_non_nullable
+              as LegendPosition,
+      titleFont: null == titleFont
+          ? _value.titleFont
+          : titleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      labelFont: null == labelFont
+          ? _value.labelFont
+          : labelFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      subTitleFont: null == subTitleFont
+          ? _value.subTitleFont
+          : subTitleFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      valueFont: null == valueFont
+          ? _value.valueFont
+          : valueFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+      tooltipFont: null == tooltipFont
+          ? _value.tooltipFont
+          : tooltipFont // ignore: cast_nullable_to_non_nullable
+              as Map<String, dynamic>,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$MultiFieldStackedAreaChartConfigImpl
+    extends _MultiFieldStackedAreaChartConfig {
+  _$MultiFieldStackedAreaChartConfigImpl(
+      {this.title = 'Stacked Area Chart',
+      this.deviceId = '',
+      this.fields = const [],
+      this.chartColors = const [],
+      this.legendVisibility = true,
+      this.enableTooltip = true,
+      this.enableDataLabel = true,
+      this.enableMarkerPoint = true,
+      this.legendPosition = LegendPosition.bottom,
+      this.titleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 18,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.labelFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 10,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.subTitleFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 12,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.valueFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 12,
+        'fontColor': 0xFF000000,
+        'fontBold': true
+      },
+      this.tooltipFont = const {
+        'fontFamily': 'Open Sans',
+        'fontSize': 12,
+        'fontColor': 0xFFFFFFFF,
+        'fontBold': true
+      }})
+      : super._();
+
+  factory _$MultiFieldStackedAreaChartConfigImpl.fromJson(
+          Map<String, dynamic> json) =>
+      _$$MultiFieldStackedAreaChartConfigImplFromJson(json);
+
+  @override
+  @JsonKey()
+  String title;
+  @override
+  @JsonKey()
+  String deviceId;
+  @override
+  @JsonKey()
+  List<String> fields;
+  @override
+  @JsonKey()
+  List<int> chartColors;
+  @override
+  @JsonKey()
+  bool legendVisibility;
+  @override
+  @JsonKey()
+  bool enableTooltip;
+  @override
+  @JsonKey()
+  bool enableDataLabel;
+  @override
+  @JsonKey()
+  bool enableMarkerPoint;
+  @override
+  @JsonKey()
+  LegendPosition legendPosition;
+  @override
+  @JsonKey()
+  Map<String, dynamic> titleFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> labelFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> subTitleFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> valueFont;
+  @override
+  @JsonKey()
+  Map<String, dynamic> tooltipFont;
+
+  @override
+  String toString() {
+    return 'MultiFieldStackedAreaChartConfig(title: $title, deviceId: $deviceId, fields: $fields, chartColors: $chartColors, legendVisibility: $legendVisibility, enableTooltip: $enableTooltip, enableDataLabel: $enableDataLabel, enableMarkerPoint: $enableMarkerPoint, legendPosition: $legendPosition, titleFont: $titleFont, labelFont: $labelFont, subTitleFont: $subTitleFont, valueFont: $valueFont, tooltipFont: $tooltipFont)';
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$MultiFieldStackedAreaChartConfigImplCopyWith<
+          _$MultiFieldStackedAreaChartConfigImpl>
+      get copyWith => __$$MultiFieldStackedAreaChartConfigImplCopyWithImpl<
+          _$MultiFieldStackedAreaChartConfigImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$MultiFieldStackedAreaChartConfigImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _MultiFieldStackedAreaChartConfig
+    extends MultiFieldStackedAreaChartConfig {
+  factory _MultiFieldStackedAreaChartConfig(
+          {String title,
+          String deviceId,
+          List<String> fields,
+          List<int> chartColors,
+          bool legendVisibility,
+          bool enableTooltip,
+          bool enableDataLabel,
+          bool enableMarkerPoint,
+          LegendPosition legendPosition,
+          Map<String, dynamic> titleFont,
+          Map<String, dynamic> labelFont,
+          Map<String, dynamic> subTitleFont,
+          Map<String, dynamic> valueFont,
+          Map<String, dynamic> tooltipFont}) =
+      _$MultiFieldStackedAreaChartConfigImpl;
+  _MultiFieldStackedAreaChartConfig._() : super._();
+
+  factory _MultiFieldStackedAreaChartConfig.fromJson(
+          Map<String, dynamic> json) =
+      _$MultiFieldStackedAreaChartConfigImpl.fromJson;
+
+  @override
+  String get title;
+  set title(String value);
+  @override
+  String get deviceId;
+  set deviceId(String value);
+  @override
+  List<String> get fields;
+  set fields(List<String> value);
+  @override
+  List<int> get chartColors;
+  set chartColors(List<int> value);
+  @override
+  bool get legendVisibility;
+  set legendVisibility(bool value);
+  @override
+  bool get enableTooltip;
+  set enableTooltip(bool value);
+  @override
+  bool get enableDataLabel;
+  set enableDataLabel(bool value);
+  @override
+  bool get enableMarkerPoint;
+  set enableMarkerPoint(bool value);
+  @override
+  LegendPosition get legendPosition;
+  set legendPosition(LegendPosition value);
+  @override
+  Map<String, dynamic> get titleFont;
+  set titleFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get labelFont;
+  set labelFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get subTitleFont;
+  set subTitleFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get valueFont;
+  set valueFont(Map<String, dynamic> value);
+  @override
+  Map<String, dynamic> get tooltipFont;
+  set tooltipFont(Map<String, dynamic> value);
+  @override
+  @JsonKey(ignore: true)
+  _$$MultiFieldStackedAreaChartConfigImplCopyWith<
+          _$MultiFieldStackedAreaChartConfigImpl>
+      get copyWith => throw _privateConstructorUsedError;
+}
